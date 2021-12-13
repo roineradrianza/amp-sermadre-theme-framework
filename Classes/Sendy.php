f0fd418d7250e0af7a031643f0740e765d5e0cee
Roiner Adrianza <roineradrianzap@gmail.com>
2021-12-13 16:36:02 -0400

    Sendy integration

---
+++ Classes/Sendy.php
@@ -0,0 +1,43 @@
+<?php
+
+SERMA_SENDY::init();
+
+use AhmadAwais\Sendy\API as Sendy;
+
+/**
+ * Sendy API
+ *
+ * SerMadre Sendy Newsletter Handler
+ *
+ * @version 0.1
+ */
+
+class SERMA_SENDY
+{
+
+    private static $_config = [
+        'sendyUrl' => SERMA_SENDY_URL,
+        'apiKey' => SERMA_SENDY_API_KEY,
+        'listId' => SERMA_SENDY_LIST,
+    ];
+
+    public static function init() {
+        //SUBSCRIBE
+        add_action('wp_ajax_serma_sendy_subscribe', 'SERMA_SENDY::subscribe');
+        add_action('wp_ajax_nopriv_serma_sendy_subscribe', 'SERMA_SENDY::subscribe');
+    }
+
+    public static function subscribe() {
+        $data = $_POST;
+        $sendy = new Sendy(self::$_config);
+        $responseArray = $sendy->subscribe(
+            [
+                'email'     => sanitize_email( $data['email'] ), // This is the only field required by sendy.
+                'ipaddress' => $_SERVER['REMOTE_ADDR'], // User IP address (optional).
+                'referrer'  => esc_url_raw( $data['url_referrer'] ), // URL where the user signed up from (optional).
+            ]
+        );
+        wp_send_json( $responseArray );
+    }
+    
+}
---
+++ config/global_template.php
@@ -0,0 +1,5 @@
+<?php
+
+define('SERMA_SENDY_LIST', '');
+define('SERMA_SENDY_URL', 'https://sendy.domain.com');
+define('SERMA_SENDY_API_KEY', '');
